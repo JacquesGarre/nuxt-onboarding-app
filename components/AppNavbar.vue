9d5bebe80a19550151c8bde98a5498f708220ff8
Jacques Garré <jcqs.gr@gmail.com>
2023-06-16 21:06:46 +0200

    page header

--- components/AppNavbar.vue
+++ components/AppNavbar.vue
@@ -1,8 +1,9 @@
 <template>
-<nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur" navbar-scroll="true">
+    <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 pt-3 shadow-none border-radius-xl" id="navbarBlur" navbar-scroll="true">
       <div class="container-fluid py-1 px-3">
-        <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
-          <div class="ms-md-auto pe-md-3 d-flex align-items-center">
+        <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4 justify-content-between" id="navbar">
+          <div class="pe-md-3 d-flex align-items-center">
+            <AppBreadcrumb/>
           </div>
           <ul class="navbar-nav  justify-content-end">
             <li class="nav-item d-flex align-items-center">
