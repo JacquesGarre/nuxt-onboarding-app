f5a839a5a2abe4d2ddea5e2fffa591185c8d9ba2
Jacques Garré <jcqs.gr@gmail.com>
2023-06-17 12:53:18 +0200

    fix active btn

--- components/AppSidebarButton.vue
+++ components/AppSidebarButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <NuxtLink :to="menuItem.url" class="nav-link">
+    <NuxtLink :to="menuItem.url" class="nav-link" :class="{ active: isActive(menuItem.url) }">
         <div
             class="icon icon-shape icon-sm shadow border-radius-md text-center me-2 d-flex align-items-center justify-content-center bg-white text-dark">
             <font-awesome-icon :icon="['fas', menuItem.icon]"/>
@@ -8,9 +8,18 @@
     </NuxtLink>
 </template>
 <script setup>
+
+    import { useRoute } from 'vue-router';
+
     const props = defineProps({
         menuItem: Object
     })
+
+    const route = useRoute();
+
+    const isActive = (path) => {
+        return route.path.startsWith(path);
+    };
 </script>
 
 
