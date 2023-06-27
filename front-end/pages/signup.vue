b58235ca7dbff674a3263d5f9bd5d67bdcdf8155
Jacques Garré <jcqs.gr@gmail.com>
2023-06-27 08:11:10 +0200

    register

--- front-end/pages/signup.vue
+++ front-end/pages/signup.vue
@@ -5,16 +5,12 @@
       <div class="page-header align-items-start min-vh-25 pt-5 pb-11 m-3 border-radius-lg" style="background-image: url('../assets/img/curved-images/curved14.jpg');">
         <span class="mask bg-gradient-dark opacity-6"></span>
         <div class="container">
-          <div class="row justify-content-center">
-            <div class="col-lg-5 text-center mx-auto">
-              <h1 class="text-white mb-2 mt-5">Welcome!</h1>
-            </div>
-          </div>
+
         </div>
       </div>
       <div class="container">
         <div class="row mt-lg-n10 mt-md-n11 mt-n10">
-          <div class="col-xl-4 col-lg-5 col-md-7 mx-auto">
+          <div class="col-xl-5 col-lg-5 col-md-7 mx-auto">
             <div class="card z-index-0">
               <div class="card-header text-center pt-4 pb-0">
                 <h3>Register</h3>
